79619fe698fe1a7bc62325f078f08d3365e2c25c
Felix Trepanier <felixtrepanier@gmail.com>
2024-09-11 14:07:01 -0400

    spring v3

---
+++ README.md
@@ -0,0 +1,5 @@
+Exécuter
+
+```
+./gradlew bootRun
+```
---
+++ src/test/java/org/inf5190/serialization/SerializationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inf5190.serialization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerializationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
